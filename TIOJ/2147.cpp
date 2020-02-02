3bdd531fc1453de6bc72cce74e2718c822c66b86
vincent97198 <vincent97198@gmail.com>
2020-02-02 14:55:57 +0800

    DP BIT optimize

--- TIOJ/2147.cpp
+++ TIOJ/2147.cpp
@@ -15,15 +15,15 @@ inline int lowbit(int x)
 
 void add(int pos,ll val)
 {
-	for(;pos>0;pos-=lowbit(pos))
+	for(;pos<N;pos+=lowbit(pos))
 		BIT[pos]=min(BIT[pos],val);
 }
 
 ll query(int pos)
 {
 	ll tmp=1e18;
-	for(;pos<n;pos+=lowbit(pos))
-		BIT[pos]=min(BIT[pos],tmp);
+	for(;pos>0;pos-=lowbit(pos))
+		tmp=min(BIT[pos],tmp);
 	return tmp;
 }
 
@@ -42,7 +42,7 @@ int main()
 		num[i]=distance(v.begin(),lower_bound(v.begin(),v.end(),num[i]))+1;
 	
 	for(int i=0;i<n;++i){
-		for(int j=0;j<num[i];++i)
+		for(int j=1;j<num[i];++j)
 			dp[0][i][j]=1e18;
 		for(int j=num[i];j<=v.size();++j)
 			dp[0][i][j]=v[j-1]-v[num[i]-1];
@@ -53,7 +53,7 @@ int main()
 		memset(BIT,0x3f,sizeof(BIT));
 		memset(dp[cur],0x3f,sizeof(dp[cur]));
 		for(int j=0;j<n;++j){
-			for(int c=num[i];c<=v.size();++c)
+			for(int c=num[j];c<=v.size();++c)
 				dp[cur][j][c]=min(dp[cur][j][c],query(c)+v[c-1]-v[num[j]-1]);
 			for(int c=1;c<=v.size();++c)
 				add(c,dp[cur^1][j][c]);
