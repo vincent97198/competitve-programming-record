6b9e9f52c9429eb44c7b6a0ea98986f9fbc00c1d
vincent97198 <vincent97198@gmail.com>
2020-02-05 00:01:31 +0800

    priority_queue

--- codeforce/1296D.cpp
+++ codeforce/1296D.cpp
@@ -1,15 +1,40 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define N 100000
+#define N 200005
 
 using namespace std;
+
+ll n,a,b,k,h;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);	cout.tie(nullptr);
 
-	
+	cin >> n  >> a >> b >> k;
+	ll ans=0;
+	priority_queue<ll,vector<ll>,greater<ll>> pq;
+	for(int i=0;i<n;++i){
+		cin >> h;
+		h%=(a+b);
+		if(h<=a && h!=0)
+			++ans;
+		else if(h==0)
+			pq.push( ceil(b/(double)a) );
+		else
+			pq.push( ceil( (h-a)/(double)a ) );
+	}
+
+	while(k>0 && !pq.empty()){
+		ll tmp=pq.top();	pq.pop();
+		if(tmp>k)
+			break;
+		else{
+			k-=tmp;
+			++ans;
+		}
+	}
+	cout << ans << endl;
 
 	return 0;
 }
