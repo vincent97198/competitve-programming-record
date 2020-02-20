556860cbeecda883869829f3d4c0891650449cd2
vincent97198 <vincent97198@gmail.com>
2020-02-20 10:37:31 +0800

    dp

---
+++ TIOJ/1973.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define N 5005
+
+using namespace std;
+
+const ll MOD=1000000007;
+ll dp[N][N];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);	cout.tie(nullptr);
+
+	char type;
+	int n,k,tmp,sum=0;
+	cin >> type >> n >> k;
+	for(int i=0;i<k;++i){
+		cin >> tmp;
+		sum+=tmp;
+	}
+
+	memset(dp,0,sizeof(dp));
+	if(type=='U'){
+		n-=sum;
+
+		dp[0][0]=1;
+		for(int i=1;i<=n;++i)
+			for(int j=1;j<=i;++j)
+				dp[i][j]=(dp[i-1][j-1]+dp[i-j][j])%MOD;
+	}
+	else{
+		dp[k][k]=1;
+		for(int i=k;i<=n;++i){
+			for(ll j=k;j<=i;++j){
+				if(i==k && j==k)
+					continue;
+				else
+					dp[i][j]=(dp[i-1][j-1]+dp[i-1][j]*j)%MOD;
+			}
+		}
+	}
+
+	ll ans=0;
+	for(int i=1;i<=n;++i)
+		ans=(ans+dp[n][i])%MOD;
+	cout << ans << endl;
+
+	return 0;
+}
