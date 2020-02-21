8f3860ed64d375f125c4cb55c2e720d221ef5dd5
vincent97198 <vincent97198@gmail.com>
2020-02-21 18:10:11 +0800

    greedy

---
+++ mrJudge/1388.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define N 2000005
+
+using namespace std;
+
+struct item{
+  int val,pos;
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);	cout.tie(nullptr);
+
+    int n,h;
+	cin >> n;
+	stack<item> st;
+	ll ans=0;
+	for(int i=0;i<n;++i){
+	    cin >> h;
+
+        while(!st.empty() && st.top().val<h)
+            st.pop();
+	    if(!st.empty())
+            ans+=(i-st.top().pos+st.size()-1);
+	    else
+	        ans+=i;
+
+	    st.push(item{h,i});
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
