dfef368b9ac2c81192aa82b16c83dc0ef5aa8f20
vincent97198 <vincent97198@gmail.com>
2020-03-03 23:17:08 +0800

    math https://blog.csdn.net/PhantomAgony/article/details/78721764

---
+++ zerojudge/e320.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define N 200005
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);	cout.tie(nullptr);
+
+    ll a,b;
+    cin >> a >> b;
+    cout << a*b-a-b << endl;
+
+    return 0;
+}
+
