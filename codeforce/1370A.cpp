a2109971b3be299724899d2bda1e5342dc657d41
vincent97198 <vincent97198@gmail.com>
2020-06-22 20:27:11 +0800

    800

---
+++ codeforce/1370A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define N 200005
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);   cout.tie(nullptr);
+
+    int t;  cin >> t;
+    while(t--){
+        int n;  cin >> n;
+        cout << n/2 << endl;
+    }
+
+    return 0;
+}
